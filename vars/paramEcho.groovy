e8133d84dec720fcf3f46c7edf26cf34ecdead8b
rameshz23 <ramehz@yah.com>
2019-01-03 20:15:01 +0530

    commite new file

---
+++ vars/paramEcho.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/env groovy
+
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    node {
+        echo "config=${config}"
+    }
+}
