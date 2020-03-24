68a486e8e8d56062fdfde9a9f5df3d7383dcd6b9
rameshz23 <rameshz23@yahoo.com>
2020-03-24 18:49:07 +0530

    Update paramEcho.groovy

--- vars/paramEcho.groovy
+++ vars/paramEcho.groovy
@@ -10,7 +10,10 @@
 #        echo "config=${config}"
 #    }
 #}
-
+import groovy.json.JsonBuilder
+import groovy.json.JsonOutput
+import groovy.io.FileType
+import groovy.json.JsonSlurper
 
 def call(Map pipelineParams) {
     // evaluate the body block, and collect configuration into the object
