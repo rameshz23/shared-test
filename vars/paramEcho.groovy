655c748f681dbefb8de03cc336d37508347aecd9
rameshz23 <rameshz23@yahoo.com>
2020-03-24 19:49:28 +0530

    Update paramEcho.groovy

--- vars/paramEcho.groovy
+++ vars/paramEcho.groovy
@@ -1,26 +1,12 @@
 #!/usr/bin/env groovy
 
-#def call(body) {
-#    def config = [:]
-#    body.resolveStrategy = Closure.DELEGATE_FIRST
-#    body.delegate = config
-#    body()
-#
-#    node {
-#        echo "config=${config}"
-#    }
-#}
-import groovy.json.JsonBuilder
-import groovy.json.JsonOutput
-import groovy.io.FileType
-import groovy.json.JsonSlurper
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
 
-pipeline {
-
-	stages {
-		stage ('sample'){
-			 sh 'echo "coming"'
-				}
-			}
-	}
-
+    node {
+        echo "config=${config}"
+    }
+}
