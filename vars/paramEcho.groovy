c527dee0297fe951e3e674e53a8ad98ba1483955
rameshz23 <rameshz23@yahoo.com>
2020-03-24 18:56:22 +0530

    Update paramEcho.groovy

--- vars/paramEcho.groovy
+++ vars/paramEcho.groovy
@@ -15,20 +15,12 @@ import groovy.json.JsonOutput
 import groovy.io.FileType
 import groovy.json.JsonSlurper
 
-def call(Map pipelineParams) {
-    // evaluate the body block, and collect configuration into the object
-    def pipelineParams= [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = pipelineParams
-    body()
-
 pipeline {
 
-	agent ('agent-jnlp')
-		stages {
+	stages {
 		stage ('sample'){
 			 sh 'echo "coming"'
 				}
 			}
 	}
-}
+
