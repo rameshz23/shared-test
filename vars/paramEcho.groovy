7cbd637abc1700ba4c79ef03069e7ad3f5f3293a
rameshz23 <rameshz23@yahoo.com>
2020-03-25 19:20:18 +0530

    Update paramEcho.groovy

--- vars/paramEcho.groovy
+++ vars/paramEcho.groovy
@@ -1,13 +1,10 @@
-node {
-   stage 'test'
-   def whatThe = someFunc('textToFunc')
-   def whatThe2 = someFunc2('textToFunc2')
-}
-def someFunc(String text){
-    echo text
-    text
-}
-def someFunc2(String text2){
-    echo text2
-    text2
+pipeline {
+  agent any
+  stages {
+    stage('Stage 1') {
+      steps {
+        echo 'Hello world!'
+      }
+    }
+  }
 }
