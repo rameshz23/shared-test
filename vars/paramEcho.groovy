cc2a8e5d39f21ecbfc53c60c03405bf3e04bdb28
rameshz23 <rameshz23@yahoo.com>
2020-03-25 12:32:12 +0530

    Update paramEcho.groovy

--- vars/paramEcho.groovy
+++ vars/paramEcho.groovy
@@ -1,12 +1,6 @@
-node ('worker_node1') {
-  stage('Source') { // Get code
-    // get code from our Git repository
-    git 'http://github.com/brentlaster/roarv2'
-  }
-  stage('Compile') { // Compile and do unit testing
-    // get Gradle HOME value
-    def gradleHome = tool 'gradle4'
-    // run Gradle to execute compile and unit testing
-    sh "'${gradleHome}/bin/gradle' clean compileJava test"
-  }
+listView('example') {
+    jobs {
+        name('build')
+        name('test')
+    }
 }
